7149d5d4373657eaa76648d2c03349480e1b1399
Jaimin214 <jaimin.joshi1998@gmail.com>
2018-07-09 18:17:31 -0400

    1

--- app/src/main/java/com/example/jaky_d/smartattendance/SplashScreen.java
+++ app/src/main/java/com/example/jaky_d/smartattendance/SplashScreen.java
@@ -4,23 +4,50 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private ProgressBar mprogress;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mprogress = (ProgressBar) findViewById(R.id.splash_screen_progressbar);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(intent);
+            new Thread(new Runnable()
+            {
+
+            public void run()
+                {
+
+                doWork();
+                startApp();
                 finish();
-            }
-        }, 3000);
-    }
+                }
+            }).start();
+
 }
 
+public void doWork()
+{
+        for (int prog=0;prog<100;prog+=25)
+        {
+            try
+            {
+                Thread.sleep(1000);
+                mprogress.setProgress(prog);
+            }   catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void startApp()
+    {
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
